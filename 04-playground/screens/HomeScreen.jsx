75c7bc82141d505112316b54190d6c0532a50f5e
MarcoKoen <neokocram@gmail.com>
2023-08-15 10:25:45 +1200

    04 updates

--- 04-playground/screens/HomeScreen.jsx
+++ 04-playground/screens/HomeScreen.jsx
@@ -1,12 +1,42 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import nbaTeams from "../data/nba-teams";
+import { SectionList, StyleSheet, Text, View, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import nbaTeams from "../data/nbaTeams";
 
 const HomeScreen = () => {
+  const getTeamsByDivision = (division) =>
+    nbaTeams.filter((team) => team.division === division);
+
+  const renderSectionHeader = ({ section }) => (
+    <Text style={styles.sectionHeader}>{section.title}</Text>
+  );
+
+  const sections = ["Atlantic", "Central", "Northwest", "Pacific", "Southeast", "Southwest"].map(division => {
+    const teams = getTeamsByDivision(division);
+    return {
+      title: division,
+      data: teams.map(team => ({
+        ...team,
+        img: team.img 
+      }))
+    };
+  });
+    
+  const renderItem = ({ item }) => (
+      <TouchableOpacity style={{ flexDirection: "row", alignItems: "center" }}>
+        <Image source={item.img} style={{ width: 30, height: 30, marginRight: 10 }} />
+        <Text style={styles.item}>{item.key}</Text>
+      </TouchableOpacity>
+    );
+  
+    console.log(sections);
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={nbaTeams}
-        renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+      <SectionList
+        sections={sections}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        keyExtractor={(item) => item.key}
       />
     </View>
   );
@@ -15,6 +45,12 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  sectionHeader: {
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+    backgroundColor: "lightgray",
   },
   item: {
     padding: 10,
