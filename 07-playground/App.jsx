26674c2677aff02781418e189a90449a3a73d366
Marco Koen <neokocram@gmail.com>
2023-09-19 19:00:57 +1200

    android studio sucks

--- 07-playground/App.jsx
+++ 07-playground/App.jsx
@@ -1,50 +1,106 @@
-import MapView , {Marker} from "react-native-maps";
-import { StyleSheet, View } from "react-native";
-import  axios  from "axios";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+  LocationAccuracy,
+} from "expo-location";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  let data = "";
+  const [location, setLocation] = useState(null);
+  const [errMsg, setErrMsg] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      try {
+        // Request permission to access location. This will prompt the user for permission
+        let { status } = await requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          setErrMsg("Permission to access location was denied");
+          return;
+        }
+
+        // Get the current location with high accuracy
+        let location = await getCurrentPositionAsync({
+          accuracy: LocationAccuracy.High,
+        });
+        setLocation(location);
+      } catch (err) {
+        setErrMsg("Error while fetching location");
+        console.log(err);
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  console.log(location)
+
   const getData = () => {
-    axios.get("https://gist.githubusercontent.com/Grayson-Orr/d72b53e4d5b094305b8303c9330908f0/raw")
-    .then(res => {
-      data = res.data;
-      console.log(data)
-    })
-  }
-  getData(); 
+    axios
+      .get("https://gist.githubusercontent.com/Grayson-Orr/d72b53e4d5b094305b8303c9330908f0/raw")
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  };
 
   const mapData = () => {
-    const mappedData = data.map((item) => (
+    return data.map((item) => (
       <Marker
-          key= {item.name}
-          coordinate={{ latitude: item.location.latitude, longitude: item.location.longitude }}
-          title={item.name}
-          description={item.city}
-        />
-    ))
-    return mappedData;
-  }
-  
-
+        key={item.name}
+        coordinate={{
+          latitude: item.location.latitude,
+          longitude: item.location.longitude,
+        }}
+        title={item.name}
+        description={item.city}
+        pinColor="yellow"
+      />
+    ));
+  };
 
   return (
     <View style={styles.container}>
-      <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <MapView
+          initialRegion={{
+            latitude: 37.49209,
+            longitude: -122.222378,
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5,
+          }}
+          style={styles.map}
+        >
+          {mapData()}
+          <Marker
+        coordinate={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
         }}
-        style={styles.map}
-      >
-        
-      </MapView>
+        title={"You are here"}
+        pinColor="blue"
+      />
+        </MapView>
+      )}
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
