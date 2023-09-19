25d76665f806701b53d5270079dbc7f81a47086b
Marco Koen <neokocram@gmail.com>
2023-09-19 15:25:05 +1200

    s

--- 07-playground/App.jsx
+++ 07-playground/App.jsx
@@ -1,8 +1,33 @@
 import MapView , {Marker} from "react-native-maps";
 import { StyleSheet, View } from "react-native";
-import { Axios } from "axios";
+import  axios  from "axios";
 
 const App = () => {
+
+  let data = "";
+  const getData = () => {
+    axios.get("https://gist.githubusercontent.com/Grayson-Orr/d72b53e4d5b094305b8303c9330908f0/raw")
+    .then(res => {
+      data = res.data;
+      console.log(data)
+    })
+  }
+  getData(); 
+
+  const mapData = () => {
+    const mappedData = data.map((item) => (
+      <Marker
+          key= {item.name}
+          coordinate={{ latitude: item.location.latitude, longitude: item.location.longitude }}
+          title={item.name}
+          description={item.city}
+        />
+    ))
+    return mappedData;
+  }
+  
+
+
   return (
     <View style={styles.container}>
       <MapView
@@ -14,11 +39,7 @@ const App = () => {
         }}
         style={styles.map}
       >
-        <Marker
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          title="Some Title"
-          description="Some Description"
-        />
+        
       </MapView>
     </View>
   );
